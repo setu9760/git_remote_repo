ca2d1f37f3eb50142678d4ff78027c85c285c495
sd12ado <sd12ado@herts.ac.uk>
2014-04-02 17:11:23 +0100

    commit

---
+++ EasyOrder/src/com/setu/EasyOrder/Splash_activity.java
@@ -0,0 +1,18 @@
+package com.setu.EasyOrder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash_activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+
+		
+	}
+
+	
+
+}
