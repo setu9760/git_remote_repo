b73f20170af3548894e29d9fbe41d02b4fe327a4
sd12ado <sd12ado@herts.ac.uk>
2014-04-02 18:00:33 +0100

    working on splash

---
+++ EasyOrder/src/com/setu/eO_GUI/Spash_activity.java
@@ -0,0 +1,16 @@
+package com.setu.eO_GUI;
+
+import com.setu.EasyOrder.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Spash_activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+	}
+
+}
