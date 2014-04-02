a19bf7db7fa41d12f8956cf49968ba665db9e4ea
sd12ado <sd12ado@herts.ac.uk>
2014-04-02 18:58:02 +0100

    Splash implemented successfully. need to work on pay now/order screen

--- EasyOrder/src/com/setu/eO_GUI/MainActivity.java
+++ EasyOrder/src/com/setu/eO_GUI/MainActivity.java
@@ -1,29 +1,19 @@
 package com.setu.eO_GUI;
 
 import com.setu.EasyOrder.R;
-import com.setu.EasyOrder.R.id;
-import com.setu.eO_Logic.*;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class MainActivity extends Activity implements OnClickListener,
-		OnCheckedChangeListener {
-	private EditText txtusername;
-	private EditText txtpassword;
+public class MainActivity extends Activity implements OnClickListener {
+
 	private Button btnlogin;
 	private Button btnreset;
 	private Button btnregister;
-	private CheckBox chkremme;
 	private ProgressDialog progress;
 
 	@Override
@@ -31,13 +21,9 @@ public class MainActivity extends Activity implements OnClickListener,
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		txtusername = (EditText) findViewById(R.id.txtusername);
-		txtpassword = (EditText) findViewById(R.id.txtpassword);
-		chkremme = (CheckBox) findViewById(R.id.chkremme);
 		btnlogin = (Button) findViewById(R.id.btnlogin);
 		btnregister = (Button) findViewById(R.id.btnregister);
 		btnreset = (Button) findViewById(R.id.btnreset);
-		chkremme = (CheckBox) findViewById(R.id.chkremme);
 
 		btnlogin.setOnClickListener(this);
 		btnregister.setOnClickListener(this);
@@ -53,9 +39,6 @@ public class MainActivity extends Activity implements OnClickListener,
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btnlogin:
-			newtask newtask = new newtask();
-			newtask.execute();
-
 			Intent intent = new Intent(MainActivity.this, Second_activity.class);
 			startActivity(intent);
 			break;
@@ -71,33 +54,8 @@ public class MainActivity extends Activity implements OnClickListener,
 
 	}
 
-	class newtask extends AsyncTask<Void, Void, Void> {
-
-		@Override
-		protected void onPreExecute() {
-			// TODO Auto-generated method stub
-			super.onPreExecute();
-			progress.show();
-		}
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			// TODO Auto-generated method stub
-			super.onPostExecute(result);
-			progress.dismiss();
-		}
-
-	}
-
 	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+	public void onBackPressed() {
+		super.onBackPressed();
 	}
-
 }
