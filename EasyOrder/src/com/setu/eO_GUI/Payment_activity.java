4f4fb8ae629b1d0fba4b20ceebd2500d2ab56912
Setu Desai <setu_desai02@yahoo.com>
2014-04-05 15:40:13 +0100

    commit

---
+++ EasyOrder/src/com/setu/eO_GUI/Payment_activity.java
@@ -0,0 +1,17 @@
+package com.setu.eO_GUI;
+
+import com.setu.EasyOrder.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Payment_activity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.payment_screen);
+		
+	}
+
+}
